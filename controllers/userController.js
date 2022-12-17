0814dd9f4e6c7e0084190c8c01282fa0df26249c
Amanda So <soamanda95@gmail.com>
2022-12-16 16:43:52 -0800

    remove email field of User Schema

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,42 @@
 const User = require("../models/user");
 
-exports.signup = (req, res) => {
-  return res.send("TBD sign up user");
-};
+const { body, validationResult } = require("express-validator");
+
+exports.signup = [
+  // validate and sanitize fields
+  body("username")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Username required")
+    .isAlphanumeric()
+    .escape()
+    .withMessage("Only letters and numbers allowed"),
+  body("email")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Email required")
+    .isEmail()
+    .escape()
+    .withMessage("Invalid email address"),
+  body("password", "Password must be at least 8 characters long")
+    .trim()
+    .isLength({ min: 8 })
+    .escape(),
+  body("password_confirmation", "Must confirm password")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+  body("password_confirmation").custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("Password confirmation does not match password.");
+    }
+    return true;
+  }),
+
+  // Process request after validation and sanitization
+];
 
 exports.login = (req, res) => {
   return res.send("TBD login user");
