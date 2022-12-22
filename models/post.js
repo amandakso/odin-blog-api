0f4bb76c0d306ff926d23c5589750bcd2015547c
Amanda So <soamanda95@gmail.com>
2022-12-21 16:55:27 -0800

    add publish date timestamp to post model

--- models/post.js
+++ models/post.js
@@ -7,6 +7,7 @@ const PostSchema = new Schema({
   content: { type: String, required: true, maxLength: 5000 },
   timestamp: { type: Date, default: Date.now },
   published: { type: Boolean, required: true, default: false },
+  publish_date: { type: Date, default: Date.now },
 });
 
 // Export model
