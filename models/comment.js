6f358c9292e03be5f0d8192a13f86c93df86a697
Amanda So <soamanda95@gmail.com>
2023-01-11 20:50:49 -0800

    include formatted dates to comment schema and include virtuals in json

--- models/comment.js
+++ models/comment.js
@@ -1,12 +1,30 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
+const opts = { toJSON: { virtuals: true } };
 
-const CommentSchema = new Schema({
-  post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
-  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  content: { type: String, required: true, maxLength: 2500 },
-  timestamp: { type: Date, default: Date.now },
-  updated: { type: Date },
+const CommentSchema = new Schema(
+  {
+    post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    content: { type: String, required: true, maxLength: 2500 },
+    timestamp: { type: Date, default: Date.now },
+    updated: { type: Date },
+  },
+  opts
+);
+
+// Format comment dates
+CommentSchema.virtual("timestamp_formatted").get(function () {
+  return this.timestamp
+    ? DateTime.fromJSDate(this.timestamp).toLocaleString(DateTime.DATE_MED)
+    : "";
+});
+
+CommentSchema.virtual("updated_formatted").get(function () {
+  return this.updated
+    ? DateTime.fromJSDate(this.updated).toLocaleString(DateTime.DATE_MED)
+    : "";
 });
 
 // Export model
