c1069f14a158a54e8d64063ec746fd4d993a6519
Amanda So <soamanda95@gmail.com>
2024-01-17 12:54:23 -0800

    add wake up timer

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cron = require("node-cron");
 
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
@@ -17,6 +18,11 @@ var usersRouter = require("./routes/users");
 var blogRouter = require("./routes/blog");
 
 require("dotenv").config();
+
+// self-ping to wake up server
+cron.schedule("*/14 * * * *", () => {
+  console.log("test wake up");
+});
 
 // Passport strategies
 passport.use(
