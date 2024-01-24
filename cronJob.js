12ab54a1a021b455b80a6c67c8e74c65fb27365b
Amanda So <soamanda95@gmail.com>
2024-01-23 17:01:57 -0800

    refactor cron job to check if current hour is between 8am and 8pm

--- cronJob.js
+++ cronJob.js
@@ -3,20 +3,26 @@ const https = require("https");
 
 const url = "https://odin-blog-api.onrender.com/blog";
 
-const job = new cron.CronJob("*/14 8-19 * * *", function () {
-  console.log("Waking up server...");
+const job = new cron.CronJob("*/14 * * * *", function () {
+  const hour = new Date().getHours();
 
-  https
-    .get(url, (res) => {
-      if (res.statusCode === 200) {
-        console.log("Server restarted");
-      } else {
-        console.error(`Error occurred with status code: ${res.statusCode}`);
-      }
-    })
-    .on("error", (err) => {
-      console.error("Error occurred during server wakeup: ", err.message);
-    });
+  if (hour > 7 && hour < 20) {
+    // 8am to 8pm
+    console.log("Waking up server...");
+    https
+      .get(url, (res) => {
+        if (res.statusCode === 200) {
+          console.log("Server restarted");
+        } else {
+          console.error(`Error occurred with status code: ${res.statusCode}`);
+        }
+      })
+      .on("error", (err) => {
+        console.error("Error occurred during server wakeup: ", err.message);
+      });
+  } else {
+    return;
+  }
 });
 
 module.exports = job;
