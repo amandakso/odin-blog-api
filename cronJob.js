37f97d654e00be3061798d3b93c00a27a348b6f0
Amanda So <soamanda95@gmail.com>
2024-02-07 12:54:05 -0800

    remove hour restriction on server wake up

--- cronJob.js
+++ cronJob.js
@@ -4,26 +4,19 @@ const https = require("https");
 const url = "https://odin-blog-api.onrender.com/blog";
 
 const job = new cron.CronJob("*/14 * * * *", function () {
-  // server 8 hours ahead of local time.
-  const hour = new Date().getHours();
+  console.log("Waking up server...");
 
-  if (hour > 16 || hour < 4) {
-    // 8am to 8pm PST => server 8 hours ahead
-    console.log(`Current hour: ${hour}. Waking up server...`);
-    https
-      .get(url, (res) => {
-        if (res.statusCode === 200) {
-          console.log("Server restarted");
-        } else {
-          console.error(`Error occurred with status code: ${res.statusCode}`);
-        }
-      })
-      .on("error", (err) => {
-        console.error("Error occurred during server wakeup: ", err.message);
-      });
-  } else {
-    console.log(`Current hour: ${hour}. Inactive hour`);
-  }
+  https
+    .get(url, (res) => {
+      if (res.statusCode === 200) {
+        console.log("Server restarted");
+      } else {
+        console.error(`Error occurred with status code: ${res.statusCode}`);
+      }
+    })
+    .on("error", (err) => {
+      console.error("Error occurred during server wakeup: ", err.message);
+    });
 });
 
 module.exports = job;
